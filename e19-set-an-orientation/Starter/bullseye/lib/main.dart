f5f610fd73eb4848bd9312868719727361b7dfe7
Icung <saiful-islam@outlook.co.id>
2023-10-14 03:03:02 +0700

    Set an Orientation

--- e19-set-an-orientation/Starter/bullseye/lib/main.dart
+++ e19-set-an-orientation/Starter/bullseye/lib/main.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
+  runApp(const BullsEyeApp());
+}
+
+class BullsEyeApp extends StatelessWidget {
+  const BullsEyeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
+    return const MaterialApp(
       title: 'Bullseye',
       home: GamePage(),
-    ),
-  );
+    );
+  }
 }
 
 class GamePage extends StatefulWidget {
